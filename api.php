551c71754ecd237a0a3d83f56a3c9db04bc4d337
taiptai <vacharapong.cw@gmail.com>
2017-01-25 15:52:23 +0700

    Update api.php

--- api.php
+++ api.php
@@ -1,21 +1,8 @@
 <?php    header ('Content-type: text/html; charset=utf-8');
 
      
-      
-      
- 
-   
-  
-        $ch = curl_init(); 
 
-        // set url สำหรับดึงข้อมูล 
-        curl_setopt($ch, CURLOPT_URL, "http://data.tmd.go.th/api/WeatherToday/V1/?type=json"); 
-
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // ตัวแปร $output เก็บข้อมูลทั้งหมดที่ดึงมา 
-        $output = curl_exec($ch); 
+        $output = curl_exec(CURLOPT_URL, "http://data.tmd.go.th/api/WeatherToday/V1/?type=json"); 
         
      
    
