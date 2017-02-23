1a82983a2028967bc3a979b8e1647ad51f6a6f8c
taiptai <vacharapong.cw@gmail.com>
2017-02-23 12:57:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,23 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text == 'who'){
 			$messages = [
+				'type' => 'text',
+				'text' => 'bot'
+			];
+			}else if($text == 'status'){
+				$messages = [
 				'type' => 'text',
 				'text' => 'ok'
 			];
+			}else{
+				$messages = [
+				'type' => 'text',
+				'text' => '?'
+			];
+				}
+					
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
