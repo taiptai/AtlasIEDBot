e4462279db7362f4ee8fc6320310b74e4c4af025
taiptai <vacharapong.cw@gmail.com>
2017-04-03 21:19:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -61,11 +61,12 @@ if (!is_null($events['events'])) {
 			
 			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos02?auth=GClHtjv2wbUwh0j:bTQbMklYoD72BlCjyvIGtOUDl';
 			$ch = curl_init($url1);
+			$post1 = json_encode($messages);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $messages);    
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post1);    
 			$response = curl_exec($ch);     
 			curl_close ($ch);     
 			return $response;
