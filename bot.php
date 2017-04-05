388421253f7186bd9c3056a8b8fccbe4099ac099
taiptai <vacharapong.cw@gmail.com>
2017-04-05 19:08:50 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			if($text == 'On'){
 			$messages = [
 				'type' => 'text',
-				'text' => 'Plug1 is on'
+				'text' => 'Test is on'
 			];
 			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos02?auth=GClHtjv2wbUwh0j:bTQbMklYoD72BlCjyvIGtOUDl';
 			$ch = curl_init($url1);
@@ -30,12 +30,12 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, "ON1");    
 			$response = curl_exec($ch);     
 			curl_close ($ch);
-			
 				
-			}else if($text == 'Off'){
+			}
+			else if($text == 'Off'){
 				$messages = [
 				'type' => 'text',
-				'text' => 'Plug1 is off'
+				'text' => 'Test is off'
 			];
 			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos02?auth=GClHtjv2wbUwh0j:bTQbMklYoD72BlCjyvIGtOUDl';
 			$ch = curl_init($url1);
@@ -45,15 +45,44 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
 			curl_setopt($ch, CURLOPT_POSTFIELDS, "OFF1");    
 			$response = curl_exec($ch);     
-			curl_close ($ch);
-			
-				
-			}else{
+			curl_close ($ch);	
+			}
+			else if($text == 'On1'){
+				$messages = [
+				'type' => 'text',
+				'text' => 'Plug1 is off'
+			];
+			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos01?auth=nbsQSyoKJEf6Zsu:VROlSCXbWzfh2XbSG8OjVNGLg';
+			$ch = curl_init($url1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "ON1");    
+			$response = curl_exec($ch);     
+			curl_close ($ch);	
+			}
+			else if($text == 'Off1'){
+				$messages = [
+				'type' => 'text',
+				'text' => 'Plug1 is off'
+			];
+			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos01?auth=nbsQSyoKJEf6Zsu:VROlSCXbWzfh2XbSG8OjVNGLg';
+			$ch = curl_init($url1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "OFF1");    
+			$response = curl_exec($ch);     
+			curl_close ($ch);	
+			}
+			else{
 				$messages = [
 				'type' => 'text',
 				'text' => 'I don`t know.'
 			];
-				}
+			}
 			
 			
 			// Make a POST Request to Messaging API to reply to sender
