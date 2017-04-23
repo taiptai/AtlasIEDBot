caf9629b29df0cb40332f8fdbad672b4fe732352
taiptai <vacharapong.cw@gmail.com>
2017-04-23 23:29:18 +0700

    Create OFF.php

---
+++ OFF.php
@@ -0,0 +1,11 @@
+<?php
+$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos02?auth=GClHtjv2wbUwh0j:bTQbMklYoD72BlCjyvIGtOUDl';
+			$ch = curl_init($url1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "OFF1");    
+			$response = curl_exec($ch);     
+			curl_close ($ch);	
+?>
