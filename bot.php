c87e09286b9abef94869a5a373245f01b88a76a8
taiptai <vacharapong.cw@gmail.com>
2017-04-26 05:21:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,6 +49,22 @@ if (!is_null($events['events'])) {
 			curl_close ($ch);	
 			}
 			
+			else if($text == 'SW'){
+				$messages = [
+				'type' => 'text',
+				'text' => 'Test is toggled'
+			];
+			$url1 = 'https://api.netpie.io/topic/testPrj/gearname/Wemos02?auth=GClHtjv2wbUwh0j:bTQbMklYoD72BlCjyvIGtOUDl';
+			$ch = curl_init($url1);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);     
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);     
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");    
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "SW1");    
+			$response = curl_exec($ch);     
+			curl_close ($ch);	
+			}
+			
 			else if($text == 'On1'){
 				$messages = [
 				'type' => 'text',
