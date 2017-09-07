20715eb824497176c6bedcb38b32e682217aa12f
taiptai <vacharapong.cw@gmail.com>
2017-09-07 16:17:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'].toLowerCase();
+			$text = $event['message']['text'];
+			$text = strtolower($text);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
